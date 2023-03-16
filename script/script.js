bcc9001c874cee522b18003e725882404a774807
2408Josue <patinojosue017@gmail.com>
2023-03-16 16:16:45 -0500

    proyecto terminado

--- script/script.js
+++ script/script.js
@@ -3,24 +3,38 @@ let nombre, correo, mensaje;
 let formulario = document.getElementById('form')
 
 formulario.addEventListener('submit',(e)=>{
+
     e.preventDefault()
-    LeerData
+    LeerData()
 })
 
 function LeerData() {
     nombre = document.getElementById('nombre').value;
     correo = document.getElementById('correo').value;
     mensaje = document.querySelector('#textareas').value;
-    console.log(nombre)
+    ValidarData(nombre, correo, mensaje)
+    GuardarLocalStore(nombre,correo,mensaje)
 }
 function ValidarData(nombre, correo, mensaje){
     if(nombre.length==0 || correo.length==0 || mensaje.length==0){
-        swall.fire({
-            title: 'Error!',
-            text: 'Do you want to continue',
-            icon: 'error',
-            confirmButtonText:'Cool',
-            iconColor:'purple'       
-        })
+        Swal.fire(
+            'The Internet?',
+            'That thing is still around?',
+            'question',
+          )
     }
+}
+
+function GuardarLocalStore(nombre,correo,mensaje) {
+    localStorage.setItem('Nombre', nombre)
+    localStorage.setItem('Correo', correo)
+    localStorage.setItem('Mensaje', mensaje)
+    ListaData()
+}
+
+
+function ListaData(){
+    let nombreUsu = localStorage.getItem('Nombre')
+    let correoUsu = localStorage.getItem('Correo')
+    let mensajeUsu = localStorage.getItem('Mensaje')
 }
